90cda26839d0ee2c8c1051eb1efe9ebcb4c6133f
tokuhirom <tokuhirom@gmail.com>
2011-08-15 15:54:01 +0900

    initial import

---
+++ cigar.pl
@@ -0,0 +1,118 @@
+#!/usr/local/bin/perl
+use strict;
+use warnings;
+use utf8;
+use 5.008008;
+
+package App::Cigar;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+use Carp ();
+use File::Spec;
+
+sub base { $_[0]->{base} }
+sub branch { $_[0]->{branch} }
+sub repo { $_[0]->{repo} }
+
+sub new {
+    my $class = shift;
+    my %args = @_==1 ? %{$_[0]} : @_;
+
+    for my $k (qw(base branch repo)) {
+        unless (defined $args{$k}) {
+            Carp::croak("missing mandatory parameter: $k");
+        }
+    }
+
+    bless { %args }, $class;
+}
+
+sub run {
+    my $self = shift;
+
+    my $branch = $self->branch;
+
+    $self->log("start testing");
+
+    {
+        chdir($self->base) or die "Cannot chdir(@{[ $self->base ]}): $!";
+        my $workdir = $self->dir("work-$branch");
+        unless (-d $workdir) {
+            $self->command("git clone --recursive @{[ $self->repo ]} $workdir");
+        }
+        chdir($workdir) or die "Cannot chdir($workdir): $!";
+        $self->command("git pull -f origin $branch");
+        $self->command("git reset --hard origin/$branch");
+        $self->command("git status");
+
+        my $report = $self->run_test();
+        if ($report) {
+            $self->notify($branch, $report);
+        }
+    }
+
+    $self->log("end testing");
+}
+
+sub notify {
+    my ($self, $branch, $report) = @_;
+    warn "NOTIFY : $branch, $report";
+}
+
+sub run_test {
+    my $self = shift;
+    if (-x './bin/test.pl') {
+        system('./bin/test.pl');
+    } else {
+        system("perl Makefile.PL")==0 or return "Cannot run Makefile.PL: $!";
+        system("make test")==0 or return "make test failed.. $?";
+    }
+}
+
+sub file {
+    my $self = shift;
+    File::Spec->catfile($self->base, @_);
+}
+
+sub dir {
+    my $self = shift;
+    File::Spec->catdir($self->base, @_);
+}
+
+sub command {
+    my $self = shift;
+    $self->log("command: @_");
+    system(@_)==0 or die "@_: $@";
+}
+
+sub log {
+    my $self = shift;
+    print join(' ', '['.$self->branch.']', @_), "\n";
+}
+
+package main;
+use Getopt::Long;
+use Pod::Usage;
+
+GetOptions(
+    'branch=s' => \my $branch,
+    'base=s'    => \my $base,
+    'repo=s'    => \my $repo,
+);
+$base or pod2usage();
+$repo or pod2usage();
+$branch='master' unless $branch;
+
+my $app =
+  App::Cigar->new( branch => $branch, base => $base, repo => $repo );
+$app->run();
+
+__END__
+
+=head1 SYNOPSIS
+
+    % cigar.pl --repo=git://... --base /path/to/base/dir
+    % cigar.pl --repo=git://... --base /path/to/base/dir --branch master --branch foo
+
