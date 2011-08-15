b872222d6e641471e98505fec84641d9303f5f59
tokuhirom <tokuhirom@gmail.com>
2011-08-15 14:30:13 +0900

    initial import

---
+++ lib/App/Cigar.pm
@@ -0,0 +1,39 @@
+package App::Cigar;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::Cigar -
+
+=head1 SYNOPSIS
+
+  use App::Cigar;
+
+=head1 DESCRIPTION
+
+App::Cigar is
+
+=head1 AUTHOR
+
+ E<lt>E<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) 
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
